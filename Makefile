0b7b40b017efd47d2fa7815dfd718eaff0dbf2b5
Graham Markall <gmarkall@nvidia.com>
2023-10-24 22:41:56 +0100

    Attempt to make a reproducer with llvm-rtdyld
    A quick attempt results in a different assertion:
    
    ```
    gmarkall@gm-agx:~/numbadev/issues/9001$ llvm-rtdyld --execute --dylib=adaptor.so --entry entry trigger.o trigger.o trigger.o trigger.o trigger.o trigger.o trigger.o trigger.o trigger.o trigger.o trigger.o trigger.o
    Allocs: 157
    llvm-rtdyld: /home/gmarkall/numbadev/llvm-project-14/llvm/lib/ExecutionEngine/RuntimeDyld/RuntimeDyldELF.cpp:451: void llvm::RuntimeDyldELF::resolveAArch64Relocation(const llvm::SectionEntry&, uint64_t, uint64_t, uint32_t, int64_t): Assertion `static_cast<int64_t>(Result) >= INT32_MIN && static_cast<int64_t>(Result) <= UINT32_MAX' failed.
    ```

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 all:
 	gcc -g -std=c11 -Wall -Werror -fPIC -shared adaptor.c -o adaptor.so
+	gcc -g -std=c11 -Wall -Werror -fPIC -c adaptor.c
+	gcc -g -std=c11 -Wall -Werror -fPIC -c trigger.c
--- adaptor.c
+++ adaptor.c
@@ -1,4 +1,8 @@
+#define _POSIX_C_SOURCE 199309L
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 // Functions referenced by the IR modules we add later
 
@@ -9,10 +13,33 @@ int numba_complex_adaptor(void *retval, void *arg) { return 1; }
 #define N_ALLOCS 5
 void* ptrs[N_ALLOCS];
 
-void mem_init()
+void set_seed (void)
 {
+    struct timespec spec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+    srand(spec.tv_nsec);
+}
+
+
+__attribute__((constructor)) void mem_init()
+{
+  set_seed();
+  int N = rand() % 512;
+  void **local_ptrs = (void**)malloc(sizeof(void*) * N);
+  printf("Allocs: %d\n", N);
+  for (size_t i = 0; i < N; i++)
+  {
+    local_ptrs[i] = calloc(10, 1024);
+  }
+
   for (size_t i = 0; i < N_ALLOCS - 1; i++)
   {
     ptrs[i] = calloc(1048576, 1024);
+  }
+
+
+  for (size_t i = 0; i < N; i++)
+  {
+    free(local_ptrs[i]);
   }
 }
